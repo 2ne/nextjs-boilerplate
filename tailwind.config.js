bfbacfb463903a8a62137af80b27d32053d5ec4f
2ne <jamestoone@gmail.com>
2022-06-19 09:52:40 +0100

    Hero, fonts, config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    corePlugins: {
+      textOpacity: false,
+      backgroundOpacity: false,
+      borderOpacity: false,
+      divideOpacity: false,
+      placeholderOpacity: false,
+      ringOpacity: false,
+    },
+    fontSize: {
+      'base': ['1.5rem', {
+        letterSpacing: '0.1em',
+        lineHeight: '2rem',
+      }],
+      'xl': ['4.75rem', {
+        letterSpacing: '0.0em',
+        lineHeight: '4.45rem',
+      }],
+    },
+    extend: {
+      colors: {
+        black: '#141414',
+      },
+      fontFamily: {
+        sans: ['Inter var', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
