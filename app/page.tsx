e335a2f5c26b1019a95cd01264a9038a0b46b7b0
James <jamestoone@gmail.com>
2023-12-28 09:59:45 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <div className="grid grid-cols-1 gap-3 xl:grid-cols-12">
           <div className="max-xl:order-0 relative overflow-clip rounded-2xl bg-gradient-to-tl from-zinc-900/75 to-zinc-900 p-6 ring-1 ring-inset ring-zinc-50/5 xl:col-span-4 xl:row-span-3 xl:grid xl:grid-rows-subgrid">
             <div className="relative mb-4 grid items-center xl:mb-0">
-              <div className="absolute -top-1 -left-1 size-3.5 bg-emerald-400 rounded-full border border-zinc-950 ring-1 ring-zinc-50/10">
+              <div className="absolute -top-1 -left-1 size-3.5 bg-zinc-950 rounded-full ring-1 ring-zinc-50/10">
                 <div className="absolute inset-px bg-gradient-to-br from-emerald-400 to-emerald-300 rounded-full animate-pulse"></div>
               </div>
               <Image alt="James Toone" width={80} height={80} className="object-cover object-left-top rounded-full size-20 bg-zinc-600 border border-zinc-950 ring-1 ring-zinc-50/10" src="/me.jpg" />
