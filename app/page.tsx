46d4c7a7a6734cefa34baceb4f8124683d974407
James <jamestoone@gmail.com>
2023-12-29 09:44:12 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -309,7 +309,7 @@ export default function Home() {
                       name="first-name"
                       id="first-name"
                       autoComplete="given-name"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-zinc-800 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-zinc-50/5 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -323,7 +323,7 @@ export default function Home() {
                       name="last-name"
                       id="last-name"
                       autoComplete="family-name"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-zinc-800 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-zinc-50/5 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -337,7 +337,7 @@ export default function Home() {
                       name="email"
                       id="email"
                       autoComplete="email"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-zinc-800 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-zinc-50/5 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -351,7 +351,7 @@ export default function Home() {
                       name="phone-number"
                       id="phone-number"
                       autoComplete="tel"
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-zinc-800 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-zinc-50/5 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -364,7 +364,7 @@ export default function Home() {
                       name="message"
                       id="message"
                       rows={4}
-                      className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-zinc-800 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-zinc-50/5 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
                       defaultValue={""}
                     />
                   </div>
